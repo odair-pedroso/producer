144bac6ee2ef64a751ffab2742859a2de5c7c568
odair-pedroso <odairpedroso90@gmail.com>
2022-06-25 14:59:21 -0300

    queue

---
+++ src/queues/base.queue.ts
@@ -0,0 +1,41 @@
+import * as Bull from 'bull';
+import { Queues } from '../enums';
+import configs from '../configs';
+
+export default class BaseQueue {
+  queue: Bull.Queue;
+
+  constructor(queue: Queues) {
+    this.queue = new Bull(queue, {
+      redis: configs.redis,
+      prefix: 'bull',
+      settings: {
+        retryProcessDelay: 500,
+      }
+    });
+    this.queue.on('failed', this.failed);
+    this.queue.on('completed', this.completed);
+    this.queue.on('error', (error) => {
+      console.error(`Falha nas tasks, verifique as configurações: ${error}`)
+    });
+  }
+  protected failed(job, err) {
+    console.error(`Queue ${job.queue.name} failed, ${job.id} - ${job.failedReason}`);
+    console.error(err);
+  }
+  protected completed(job) {
+    console.log(`Queue ${job.queue.name} completed, ${job.id}`);
+  }
+
+  add(body: any, opts?: Bull.JobOptions) {
+    return this.queue.add(
+      body,
+      opts || {
+        attempts: 5,
+        delay: 2000,
+        removeOnFail: false,
+        backoff: 5000,
+      }
+    )
+  }
+}
